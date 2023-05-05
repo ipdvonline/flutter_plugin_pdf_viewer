74c636b7ffb9a2e3546af95fe21cc9ce7826ccb4
Anderson Nunes <anderson.nunes@ipdvonline.com.br>
2023-05-05 16:19:56 -0300

    aumenta limite de zoom

--- lib/src/page.dart
+++ lib/src/page.dart
@@ -43,10 +43,10 @@ class _PDFPageState extends State<PDFPage> {
     return Container(
         decoration: null,
         child: ZoomableWidget(
-          zoomSteps: 3,
+          zoomSteps: 10,
           minScale: 1.0,
           panLimit: 0.8,
-          maxScale: 3.0,
+          maxScale: 30.0,
           child: Image(image: provider),
         ));
   }
